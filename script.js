3f3d113f53d27a06bd0afb913213a4a0ffe7580c
DrorMaman <drormaman@gmail.com>
2020-08-05 09:42:19 +0300

    code improvment

--- script.js
+++ script.js
@@ -12,40 +12,31 @@ const API_URL = 'https://pokeapi.co/api/v2';
 axios.defaults.baseURL = API_URL;
 
 
-function getPokemon(pokeIdentifier) {
-    return axios.get(`/pokemon/${pokeIdentifier}`)
-        .then(response => response.data)
-        .catch((error) => { alert("Pokemon not found\n" + error.message); })
-}
+
 
 function addImageToDocument(pokemonData) {
     pokeImg.src = pokemonData.sprites.front_default;
     pokeImg.addEventListener('mouseenter', () => {
-        pokeImg.src = pokemonData.sprites.back_default;
-    })
+        pokeImg.src = pokemonData.sprites.back_default || pokemonData.sprites.front_default;
+    });
     pokeImg.addEventListener('mouseleave', () => {
         pokeImg.src = pokemonData.sprites.front_default;
-    })
+    });
 }
 
-function addNameToDocument(pokemonData) {
-    const nameSplit = pokemonData.name.split("");
-    nameSplit[0] = nameSplit[0].toUpperCase();
-    pokeName.innerText = nameSplit.join("");
-}
-
-function addHeightToDocument(pokemonData) {
+function addPropertiesToDocument(pokemonData) {
+    pokeName.innerText = pokemonData.name;
     pokeHeight.innerText = `${pokemonData.height * 10}`;
-}
-
-function addWeightToDocument(pokemonData) {
     pokeWeight.innerText = `${pokemonData.weight * 0.1}`;
+
 }
 
 function addTypesListToDocument(pokemonData) {
+    typesUl.innerHTML = "";
     for (const type of pokemonData.types) {
         const typeLi = document.createElement('li');
-        typeLi.innerText = type.type.name;
+        const typeName = type.type.name;
+        typeLi.innerText = typeName;
         typeLi.addEventListener('click', showRelatedTypePokemons)
         typesUl.appendChild(typeLi);
     }
@@ -56,28 +47,45 @@ async function showRelatedTypePokemons(event) {
     const parentLi = event.target;
     const relatedUl = document.createElement('ul');
     parentLi.appendChild(relatedUl);
-    const relatedTypeList = await fetch(`${API_URL}/type/${parentLi.innerText}`)
+    const obj = await getListOfSameType(parentLi.innerText)
+
+    console.log(obj.pokemon)
+    const relatedTypeList = await fetch(`${API_URL}/type/${parentLi.innerText.toLowerCase()}`)
         .then(response => response.json())
         .then((data) => data.pokemon)
         .catch((error) => { alert("Pokemon not found\n" + error.message); })
     for (const pokemon of relatedTypeList) {
         const pokemonLi = document.createElement('li');
         pokemonLi.innerText = pokemon.pokemon.name;
-        pokemonLi.addEventListener('click', showPokemon)
+        pokemonLi.addEventListener('click', showPokemon);
         relatedUl.appendChild(pokemonLi);
     }
 }
 
-async function showPokemon(event) {
-    event.stopPropagation();
-    const identifier = (event.target.id === "searchButon") ? searchInput.value : event.target.innerText;
-    const pokemonObj = await getPokemon(identifier);
+// function getPokemon(pokeIdentifier) {
+//     return axios.get(`/pokemon/${pokeIdentifier.toLowerCase()}`)
+//         .then(response => response.data)
+//         .catch(error => error)
+// }
 
-    addImageToDocument(pokemonObj);
-    addNameToDocument(pokemonObj);
-    addHeightToDocument(pokemonObj);
-    addWeightToDocument(pokemonObj);
-    addTypesListToDocument(pokemonObj);
+function getListOfSameType(type) {
+    return responseData = axios.get(`/type/${type.toLowerCase()}`).then(r => r.data);
+
+}
+
+async function showPokemon(event) {
+    try {
+        event.stopPropagation();
+        const identifier = (event.target.id === "searchButon") ? searchInput.value : event.target.innerText;
+        const response = await fetch(`${API_URL}/pokemon/${identifier.toLowerCase()}`);
+        const pokemonData = await response.json()
+        addImageToDocument(pokemonData);
+        addPropertiesToDocument(pokemonData);
+        addTypesListToDocument(pokemonData);
+    } catch (error) {
+        alert("Pokemon not found helllllooo\n" + error.message);
+    }
+
 }
 
 
